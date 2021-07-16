febc201c106c8c538b018b60eface898b2263912
LeoVgr <38160869+LeoVgr@users.noreply.github.com>
2021-07-16 11:44:02 +0200

    [FEATURE] Main Menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,17 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject Menu;
     public GameObject CreditMenu;
 
     // Start is called before the first frame update
     void Start()
     {
+        Menu.SetActive(true);
         CreditMenu.SetActive(false);
     }
 
     public void StartGame()
     {
 
+    }
+    public void ShowCredit()
+    {      
+        CreditMenu.SetActive(true);
+        Menu.GetComponent<Animator>().SetTrigger("Out");
+        CreditMenu.GetComponent<Animator>().SetTrigger("In");
+    }
+    public void HideCredit()
+    {
+        Menu.GetComponent<Animator>().SetTrigger("In");
+        CreditMenu.GetComponent<Animator>().SetTrigger("Out");
     }
     public void QuitGame()
     {
